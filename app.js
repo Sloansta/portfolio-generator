bf47d2a5b487411d0e084b7ea4a26130dd1a5ae2
Sloan <Sloan.boyce@gmail.com>
2021-11-22 21:07:19 -0500

    added the usage of inquirer

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const fs = require( 'fs');
-const generatePage = require('./src/page-template.js');
+/*const fs = require( 'fs');
+const generatePage = require('./src/page-template');
 
-const commandLineArgs = process.argv.slice(2, process.argv.length);
 
-const [name, github] = commandLineArgs;
-
+const pageHTML = generatePaged(name, html);
 //console.log(commandLineArgs)
 
 fs.writeFile('./index.html', generatePage(name, github), err => {
     if (err) throw err;
 
     console.log( 'Portfolio complete!');
-});
+});*/
+const inq = require('inquirer');
 
+
+inq.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers => console.log(answers));
